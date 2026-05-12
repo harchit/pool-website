46aafd87dd48dfa930cef6b32d6829b0b13c960f
[dyad] <git@dyad.sh>
2026-05-11 21:41:42 -0400

    [dyad] Fix missing Services.tsx import error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
@@ -24,7 +23,6 @@ const App = () => (
         <StickyCallButton />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/services" element={<Services />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/vinyl-pools" element={<VinylPools />} />
